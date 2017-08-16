4edb60894b8f47f6fec0f9d0cd691758cba1aad0
Stefan Candan <justcandan@gmail.com>
2017-08-16 13:59:44 +0200

    Material design first revision

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     useref = require('gulp-useref'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
-    minifyCss = require('gulp-clean-css');
+    minifyCss = require('gulp-clean-css'),
+    imageMin = require('gulp-imagemin');
 
-gulp.task('html', function () {
-    return gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', minifyCss()))
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('html', () =>
+	gulp.src('app/*.html')
+		.pipe(useref())
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.css', minifyCss()))
+		.pipe(gulp.dest('dist'))
+);
+
+gulp.task('image', () =>
+	gulp.src('app/images/*[.jpg|.png]')
+		.pipe(imageMin())
+		.pipe(gulp.dest('dist/images'))
+);
